54070205d3f70917251faf0fa5c6ad9ac412e022
Tracey Birch <tracey.birch@sofwerx.org>
2017-10-26 08:48:35 -0400

    Put LocateTarget response into JSON

--- LocateTarget/views.py
+++ LocateTarget/views.py
@@ -2,6 +2,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.http.request import HttpRequest
+import json
 
 from swx.triangulator import Triangulator
 
@@ -9,7 +10,10 @@ from swx.triangulator import Triangulator
 def LocateTarget(request):
     
     t = Triangulator.TargetLoc()
-    targetLoc = t.locate(request.body.decode("utf-8"))
+    [hasIntersect, (lat, lon)] = t.locate(request.body.decode("utf-8"))
+    
+    targetLoc = json.dumps({"hasIntersect": hasIntersect, "targetLoc": {"lat": lat, "lon": lon }})
+    
     resp = HttpResponse(targetLoc)
     resp.__setitem__("Content-Type", "application/json")
     
