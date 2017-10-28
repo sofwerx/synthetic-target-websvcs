f4d2250d58ab2c1786d6416a4e2faae22e42576b
Ian Blenke <ian@blenke.com>
2017-10-28 00:18:23 -0400

    Add logging to LocateTarget

--- LocateTarget/views.py
+++ LocateTarget/views.py
@@ -6,10 +6,16 @@ import json
 
 from swx.triangulator import Triangulator
 
+import logging
+
+logger = logging.getLogger("django")
+
+
 @csrf_exempt
 def LocateTarget(request):
     
     t = Triangulator.TargetLoc()
+    logger.info("LocateTarget body=%s",request.body)
     targetLoc = t.locate(json.loads(request.body.decode("utf-8")))
     resp = HttpResponse(json.dumps(targetLoc))
     resp.__setitem__("Content-Type", "application/json")
