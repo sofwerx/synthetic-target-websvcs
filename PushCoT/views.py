41c2ea2ddd95da568119980f3cb4b9b5154e8531
Ian Blenke <ian@blenke.com>
2017-11-02 14:01:28 -0400

    Adding support for ATAK_PROTO

--- PushCoT/views.py
+++ PushCoT/views.py
@@ -9,8 +9,8 @@ import json
 import urllib
 
 ATAK_IP = os.getenv("ATAK_IP", "192.168.1.160")
-
 ATAK_PORT = int(os.getenv("ATAK_PORT", "4242"))
+ATAK_PROTO = int(os.getenv("ATAK_PROTO", "UDP"))
 
 @csrf_exempt
 def PushCoT(request):
@@ -25,10 +25,16 @@ def PushCoT(request):
     if "atak_port" in req:
       port = int(req["atak_port"])
 
+    proto = ATAK_PROTO
+    if "atak_proto" in req:
+      proto = int(req["atak_proto"])
+
     cot = CoT.CursorOnTarget()
     target = cot.atoms(req)
-    cot.pushTCP(ip_address, port, target)
-    #cot.pushUDP(ip_address, port, target)
+    if atak_proto == "UDP":
+      cot.pushUDP(ip_address, port, target)
+    if atak_proto == "TCP":
+      cot.pushTCP(ip_address, port, target)
     json_wrapper = {
         "cot_xml": urllib.quote(target)
     }
